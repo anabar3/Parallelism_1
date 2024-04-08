83420a577c3f6005c6f10c98d652840508aadd94
JaviCarb <javier.carballal.morgade@udc.es>
2024-04-08 18:38:46 +0200

    chatgpt

--- p1_parallelism.c
+++ p1_parallelism.c
@@ -14,16 +14,13 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
     while (1) {
-        if (rank == 0) { //If we are the master process, get n and send to the others
+        if (rank == 0) { //If we are the master process, get n
             printf("Enter the number of intervals: (0 quits) \n");
             scanf("%d", &n);
-            for (i = 1; i < numprocs; i++) {
-                MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            }
         }
-        else { //If we are not the master process, receive n
-            MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+
+        //Broadcast n to all processes
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         if (n == 0) break;
 
@@ -34,24 +31,15 @@ int main(int argc, char *argv[]) {
             sum += 4.0 / (1.0 + x * x);
         }
 
-        if (rank != 0) { //If we are not the master process, send the result of all the calculations to the master
-            MPI_Send(&sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-        }
-        else {
-            for (i = 1; i < numprocs; i++) { //If we are the master process, receive the results from the other processes
-                double temp;
-                MPI_Recv(&temp, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                sum += temp;
-            }
-            pi = h * sum;
+        //Collective operation to sum all the results
+        MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            pi *= h;
             printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
         }
     }
 
-    MPI_Finalize(); //End all processes
+    MPI_Finalize();
     return 0;
 }
-
-
-patata ddddddddd
-
