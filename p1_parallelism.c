9794839ab008095398075a8d55c845112867236b
JaviCarb <javier.carballal.morgade@udc.es>
2024-04-15 08:47:06 +0200

    ex2 with new function

--- p1_parallelism.c
+++ p1_parallelism.c
@@ -2,6 +2,24 @@
 #include <math.h>
 #include <mpi.h>
 
+int MPI_FlattreeCollective(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm) {
+    int rank, numprocs, i;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &numprocs);
+    if (rank != root) { //If we are not the root process, send the result of all the calculations to the root
+        MPI_Send(&sendbuf, count, datatype, root, 0, MPI_COMM_WORLD);
+    }
+    else {
+        for (i = 1; i < numprocs; i++) { //If we are the root process, receive the results from the other processes
+            void *temp;
+            MPI_Recv(&temp, count, datatype, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            //Apply the operation to the result
+
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     //Initial variables
     int i, n, rank, numprocs;
